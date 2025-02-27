d94622048a0fa9e40fd39602e3925f453cac2b1b
madhav1602 <madhavaggarwal580@gmail.com>
2025-02-27 11:32:09 +0530

    [Madhav] Adding two numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers{
+
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+
+//Take input for the first number
+int firstNumber=input.nextInt();
+
+//Take input for the second number
+int secondNumber=input.nextInt();
+
+//Print the addition of numbers
+System.out.print(firstNumber + secondNumber);
+}
+}
